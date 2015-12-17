288bb44f202184db3cfba6aa520df969eb899bc2
Gautier Colajanni <gautier.colajanni@gmail.com>
2015-12-17 15:54:32 -0500

    Fix dependencies and typo

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var logger = require('node-simple-logger');
 
 var express    = require('express');
 var bodyParser = require('body-parser');
-var apidocs    = require('express-api-docs');
 
 var config = {
 	'file': './router.js',
@@ -20,9 +19,6 @@ app.use(bodyParser.urlencoded({extended:false})); // to support URL-encoded bodi
 // Add routes to express
 var routes = require(config.file);
 routes.setup(app);
-
-// Generating API documentation
-apidocs.generate(config.file, config.docPath);
 
 // Opening the API
 app.listen(config.port, 'localhost', function() {
